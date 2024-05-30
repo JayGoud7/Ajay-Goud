1de030184997f9d4336a2dd96cc89fcca15b13f6
Ajay goud <ajaygoud@Ajays-Air.bbrouter>
2024-05-30 23:04:27 +0530

    added nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,17 +1,41 @@
 function NAV() {
+  const handleProjectClick = (e) => {
+    e.preventDefault();
+
+    document.querySelector(".pr-header").scrollIntoView({ behavior: "smooth" });
+  };
+
+  const handleAboutClick = (e) => {
+    e.preventDefault();
+
+    document.querySelector(".section2").scrollIntoView({ behavior: "smooth" });
+  };
+
+  const handleContactClick = (e) => {
+    e.preventDefault();
+
+    document.querySelector(".footer").scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div className="nav-bar">
       <h2>Ajay Goud</h2>
 
       <ul>
         <li>
-          <a href="#1">About</a>
+          <a href="#2" onClick={handleAboutClick}>
+            About
+          </a>
         </li>
         <li>
-          <a href="#2">Projects</a>
+          <a href="#2" onClick={handleProjectClick}>
+            Projects
+          </a>
         </li>
         <li>
-          <a href="#3">Contact</a>
+          <a href="#3" onClick={handleContactClick}>
+            Contact
+          </a>
         </li>
       </ul>
     </div>
